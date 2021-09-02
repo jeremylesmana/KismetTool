b32e341899eacddb15a11240e3f7dc538e24e898
Jeremy Lesmana <jeremylesmana@gmail.com>
2021-09-01 22:24:05 -0700

    Added some stuff

--- functions.js
+++ functions.js
@@ -7,23 +7,67 @@ var locSupport = "";
 var locType = "";
 var locInit = "";
 var locEachTurn = "";
+
 var locToggle = false;
 
+//action variables
+
+var actName = "";
+var actPart = new Array();
+
+var actToggle = false;
+
 $(document).ready(function() {
-    $("#location").hide();
+    $("#locationDiv").hide();
+    $("#actionDiv").hide();
 });
 
-function showLocation() {
-  if (!locToggle) {
-    $("#location").show();
-    locToggle = 1;
+//Reset functions
+function resetAll() {
+  currentKismet = "";
+  locName = "";
+  locSupport = "";
+  locType = "";
+  locInit = "";
+  locEachTurn = "";
 
+  actName = "";
+  actPart = new Array();
+  updateKismet();
+}
+
+function hideAll() {
+  locToggle = false;
+  actToggle = false;
+  $("#locationDiv").hide();
+  $("#actionDiv").hide();
+}
+
+//Toggle functions
+function toggleLocation() {
+  if (!locToggle) {
+    hideAll();
+    $("#locationDiv").show();
+    locToggle = true;
   }
   else {
-    $("#location").hide();
-    locToggle = 0;
+    $("#locationDiv").hide();
+    locToggle = false;
   }
 }
+
+function toggleAction() {
+  if (!actToggle) {
+    hideAll();
+    $("#actionDiv").show();
+    actToggle = true;
+  }
+  else {
+    $("#actionDiv").hide();
+    actToggle = false;
+  }
+}
+
 
 function locTypeFunc() {
   locType = document.getElementById("locType").value;
