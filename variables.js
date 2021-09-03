953bf42eeecb688b791624bfd161bcd3355b5453
Jeremy Lesmana <jeremylesmana@gmail.com>
2021-09-03 14:22:36 -0700

    Separated the main js file to 3 different files to avoid messy and long code

---
+++ variables.js
@@ -0,0 +1,25 @@
+/* Variables script
+  Here is where the initial variables are located.
+  Also able to be accessed by other scripts in the same webpage.
+ */
+var currentKismet ="";
+
+//location variables
+
+var locName = "";
+var locSupport = "";
+var locType = "";
+var locInit = "";
+var locEachTurn = "";
+
+var locToggle = false;
+
+//action variables
+
+var actName = "";
+var actParts = new Array();
+var actPartCounter = 1;
+var actTagCounter = 1;
+var actCondCounter = 1;
+
+var actToggle = false;
